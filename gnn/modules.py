15629ab5e78b7bcb6a84319534f78d3930e96fe7
Siyu Zhou <siyu.zhou@outlook.com>
2020-06-15 21:58:24 -0700

    Clean up.

--- gnn/modules.py
+++ gnn/modules.py
@@ -90,10 +90,7 @@ class NodePropagator(keras.layers.Layer):
 
         msg_from_source = tf.repeat(tf.expand_dims(node_states, 2), num_nodes, axis=2)
         msg_from_target = tf.repeat(tf.expand_dims(node_states, 1), num_nodes, axis=1)
-        # msg_from_source = tf.transpose(tf.tensordot(node_states, self._edge_sources, axes=[[1], [1]]),
-        #                                perm=[0, 2, 1])
-        # msg_from_target = tf.transpose(tf.tensordot(node_states, self._edge_targets, axes=[[1], [1]]),
-        #                                perm=[0, 2, 1])
+
         # msg_from_source and msg_from_target in shape [batch, num_nodes, num_nodes, out_units]
         node_msgs = tf.concat([msg_from_source, msg_from_target], axis=-1)
 
